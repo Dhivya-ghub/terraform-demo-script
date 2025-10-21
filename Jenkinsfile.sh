254bb58275a186ad716d1ecf9c437e9a3e9e1d14
Dhivya Kuttiannan <90086855+Dhivya-ghub@users.noreply.github.com>
2025-10-21 18:31:52 -0500

    Update Jenkinsfile.sh

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -9,8 +9,7 @@ pipeline {
     environment {
         AWS_ACCESS_KEY_ID     = credentials('aws-access-key-id')
         AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
-        AWS_DEFAULT_REGION    = 'us-east-2'
-        EC2_KEY = credentials('ec2-key-pem')
+        AWS_DEFAULT_REGION    = 'ap-south-1'
     }
 
     stages {
@@ -33,17 +32,15 @@ pipeline {
             }
         }
 
-        stage('Apply / Destroy') {
-            when {
-                anyOf {
-                    expression { params.autoApprove }
-                    expression { params.action == 'destroy' }
-                }
-            }
+        stage('Apply or Destroy') {
             steps {
                 script {
                     if (params.action == 'apply') {
-                        sh 'terraform apply -auto-approve tfplan'
+                        if (params.autoApprove) {
+                            sh 'terraform apply -auto-approve tfplan'
+                        } else {
+                            sh 'terraform apply tfplan'
+                        }
                     } else if (params.action == 'destroy') {
                         sh 'terraform destroy -auto-approve'
                     }
