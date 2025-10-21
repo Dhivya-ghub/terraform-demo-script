bb080e50ce6925a2082789f7245b9b45e3f587f3
Dhivya Kuttiannan <90086855+Dhivya-ghub@users.noreply.github.com>
2025-10-21 18:15:47 -0500

    Update Jenkinsfile.sh

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -1,37 +1,53 @@
 pipeline {
     agent any
+
     parameters {
         booleanParam(name: 'autoApprove', defaultValue: false, description: 'Automatically run apply after generating plan?')
         choice(name: 'action', choices: ['apply', 'destroy'], description: 'Select the action to perform')
-
     }
- environment {
+
+    environment {
         AWS_ACCESS_KEY_ID     = credentials('aws-access-key-id')
         AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
-        AWS_DEFAULT_REGION    = 'ap-south-1'
+        AWS_DEFAULT_REGION    = 'us-east-2'
     }
-  stages {
-     stage('checkout')
-       steps {
-          git branch : 'main' , url: 'https://github.com/Dhivya-ghub/terraform-demo-script.git'
-       }
-     stage('Terraform init') {
-      steps {
-        sh 'terraform init'
-      }
-     }  
-     stage('plan') {
-      steps {
-        sh 'terraform plan -out tfplan'
-        sh 'terrafrm show -no-color tfplan >tfplan.txt'
-       }
-     } 
-      stage('Apply / Destroy') {
-            steps {
-             sh 'terraform apply'
-                  }
-                }
-              
 
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Dhivya-ghub/terraform-demo-script.git'
+            }
+        }
+
+        stage('Terraform Init') {
+            steps {
+                sh 'terraform init'
+            }
+        }
+
+        stage('Terraform Plan') {
+            steps {
+                sh 'terraform plan -out=tfplan'
+                sh 'terraform show -no-color tfplan > tfplan.txt'
+            }
+        }
+
+        stage('Apply / Destroy') {
+            when {
+                anyOf {
+                    expression { params.autoApprove }
+                    expression { params.action == 'destroy' }
+                }
+            }
+            steps {
+                script {
+                    if (params.action == 'apply') {
+                        sh 'terraform apply -auto-approve tfplan'
+                    } else if (params.action == 'destroy') {
+                        sh 'terraform destroy -auto-approve'
+                    }
+                }
+            }
+        }
     }
-}     
+}
