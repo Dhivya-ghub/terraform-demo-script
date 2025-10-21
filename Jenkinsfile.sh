cdf9f46e277a2e0a94f3fec0769e768faab6be3e
Dhivya Kuttiannan <90086855+Dhivya-ghub@users.noreply.github.com>
2025-10-21 15:47:08 -0500

    Update Jenkinsfile.sh

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -1,1 +1,20 @@
+pipeline {
+    agent any
+    parameters {
+        booleanParam(name: 'autoApprove', defaultValue: false, description: 'Automatically run apply after generating plan?')
+        choice(name: 'action', choices: ['apply', 'destroy'], description: 'Select the action to perform')
 
+    }
+ environment {
+       
+        AWS_ACCESS_KEY_ID     = credentials('aws-access-key-id')
+        AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
+        AWS_DEFAULT_REGION    = 'ap-south-1'
+    }
+   stages {
+     stage('checkout')
+       steps {
+          git branch : 'main' , url: 'https://github.com/Dhivya-ghub/terraform-demo-script.git'
+       }
+     stage('Terraform init')
+     
