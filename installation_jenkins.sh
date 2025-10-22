25339f39ccafcb82b555da5ec77cd3d8c3d8c04e
Dhivya Kuttiannan <90086855+Dhivya-ghub@users.noreply.github.com>
2025-10-22 17:16:57 -0500

    Update installation_jenkins.sh

--- installation_jenkins.sh
+++ installation_jenkins.sh
@@ -17,15 +17,15 @@ sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
 # Install Docker
-#sudo apt-get install -y ca-certificates curl gnupg lsb-release
-#sudo mkdir -p /etc/apt/keyrings
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#echo \
- # "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
- # https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
- # sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#sudo apt-get update -y
-#sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-#sudo usermod -aG docker jenkins
-#sudo systemctl restart docker
-#sudo systemctl restart jenkins
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker jenkins
+sudo systemctl restart docker
+sudo systemctl restart jenkins
